439a08d922024989115529617333c9af1b1b4584
Carlos Rosales <Carlosrosales21092002@hotmail.com>
2023-02-19 17:34:55 -0500

    Editar y eliminar. Agrega una configuracion nueva a webConfig

--- Proyecto Crud/Controllers/ContactoController.cs
+++ Proyecto Crud/Controllers/ContactoController.cs
@@ -51,5 +51,82 @@ namespace Proyecto_Crud.Controllers
             return View(oLista);
         }
 
+        [HttpGet]
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Editar(int? idContacto) //El signo de interrogacion es que va a aceptar valores nulos
+        {
+            if (idContacto == null)
+            {
+                return RedirectToAction("Inicio", "Contacto");
+            }
+            Contacto ocontacto = oLista.Where(c => c.IdContact == idContacto).FirstOrDefault();
+            return View(ocontacto);
+        }
+        [HttpGet]
+        public ActionResult ELiminar(int? idContacto) //El signo de interrogacion es que va a aceptar valores nulos
+        {
+            if (idContacto == null)
+            {
+                return RedirectToAction("Inicio", "Contacto");
+            }
+            Contacto ocontacto = oLista.Where(c => c.IdContact == idContacto).FirstOrDefault();
+            return View(ocontacto);
+        }
+
+        [HttpPost]
+        public ActionResult Eliminar(string IdContact)
+        {
+            using (SqlConnection oconexion = new SqlConnection(conexion))
+            {
+                SqlCommand cmd = new SqlCommand("sp_EliminarContacto", oconexion);
+                cmd.Parameters.AddWithValue("IdContact", IdContact);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+                oconexion.Open();
+                cmd.ExecuteNonQuery();
+            }
+            return RedirectToAction("Inicio", "Contacto");
+        }
+        [HttpPost]
+        public ActionResult Editar(Contacto oContacto)
+        {
+            using (SqlConnection oconexion = new SqlConnection(conexion))
+            {
+                SqlCommand cmd = new SqlCommand("sp_EditarContacto", oconexion);
+                cmd.Parameters.AddWithValue("IdContact", oContacto.IdContact);
+                cmd.Parameters.AddWithValue("Nombre", oContacto.Nombre);
+                cmd.Parameters.AddWithValue("Apellidos", oContacto.Apellidos);
+                cmd.Parameters.AddWithValue("Telefono", oContacto.Telefono);
+                cmd.Parameters.AddWithValue("Correo", oContacto.Correo);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+                oconexion.Open();
+                cmd.ExecuteNonQuery();
+            }
+            return RedirectToAction("Inicio", "Contacto");
+        }
+        [HttpPost]
+        public ActionResult Registrar(Contacto oContacto)
+        {
+            using (SqlConnection oconexion = new SqlConnection(conexion))
+            {
+                SqlCommand cmd = new SqlCommand("sp_Registrar", oconexion);
+                cmd.Parameters.AddWithValue("Nombre", oContacto.Nombre);
+                cmd.Parameters.AddWithValue("Apellido", oContacto.Apellidos);
+                cmd.Parameters.AddWithValue("Telefono", oContacto.Telefono);
+                cmd.Parameters.AddWithValue("Correo", oContacto.Correo);
+
+                cmd.CommandType = CommandType.StoredProcedure;
+                oconexion.Open();
+                cmd.ExecuteNonQuery();
+            }
+            return RedirectToAction("Inicio", "Contacto");
+        }
+
     }
 }
